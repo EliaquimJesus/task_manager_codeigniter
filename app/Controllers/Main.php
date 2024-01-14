a285ba8ae3badd73dba98f7501681c6b44fd9c59
Eliaquim Jesus Luz <eliaquim.daluz@yahoo.com>
2024-01-14 14:44:00 +0000

    CI_06

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -3,13 +3,28 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\TasksModel;
+use App\Models\UtilizadoresModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class Main extends BaseController
 {
     public function index()
     {
-        //
-        echo "Hello World!";
+        // Utilizadores Model
+        $userModel = new UtilizadoresModel();
+        $users = $userModel->findAll();
+        
+        //dd($users);
+        echo '<pre>';
+        print_r($users);
+
+        // Tasks Model
+        $taskModel = new TasksModel();
+        $tasks = $taskModel->findAll();
+
+        //dd($tasks);
+        echo '<pre>';
+        print_r($tasks);
     }
 }
