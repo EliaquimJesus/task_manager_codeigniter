df6181c940037fa9de856836f48cb8db4babe9e9
Eliaquim Jesus Luz <eliaquim.daluz@yahoo.com>
2024-01-15 15:16:57 +0000

    CI_11

--- app/Views/login_frm.php
+++ app/Views/login_frm.php
@@ -13,16 +13,20 @@
                     <div class="mb-3">
                         <input type="text" class="form-control" name="text_utilizador" placeholder="Utilizador"
                             required>
+                        <!-- Error -->
+                        <?= !empty($validation_errors['text_utilizador']) ? '<p class="text-danger">' . $validation_errors['text_utilizador'] . '</p>' : '' ?>
                     </div>
                     <div class="mb-3">
                         <input type="password" class="form-control" name="text_password" placeholder="Password"
                             required>
+                        <!-- Error -->
+                        <?= !empty($validation_errors['text_password']) ? '<p class="text-danger">' . $validation_errors['text_password'] . '</p>' : '' ?>
                     </div>
                     <div class="mb-3">
                         <button class="btn btn-primary w-100">Entrar</button>
                     </div>
                     <?= form_close() ?>
-                    <?php if (!empty($validation_errors)) : ?>
+                    <!-- <?php if (!empty($validation_errors)) : ?>
                     <div class="alert alert-danger">
                         <ul>
                             <?php foreach ($validation_errors as $error) : ?>
@@ -30,7 +34,7 @@
                             <?php endforeach; ?>
                         </ul>
                     </div>
-                    <?php endif; ?>
+                    <?php endif; ?> -->
                 </div>
             </div>
         </div>
